86ac399120e394c374dd9cd172acea850b95ba08
evenm88 <50820787+evenm88@users.noreply.github.com>
2022-05-08 01:52:26 +0530

    Add files via upload
    Initial commit

---
+++ empApi/Controllers/EmployeesController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationEmp.Model;
+using WebApplicationEmp.Service;
+
+namespace WebApplicationEmp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeesController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Employee>> Get()
+        {
+            return _employeeService.Get();
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetEmployee")]
+        public IActionResult Get(string id)
+        {
+            var emp = _employeeService.Get(id);
+
+            if (emp == null)
+            {
+                return NotFound("No match found");
+            }
+
+            return Ok(emp);
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult AddEmployee(Employee emp)
+        {
+            try
+            {
+                 _employeeService.AddEmployee(emp);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("[action]")]
+        public IActionResult DeleteEmployee(string id)
+        {
+            try
+            {
+                _employeeService.DeleteEmployee(id);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ empApi/Model/EmployeeDatabasesetting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationEmp.Model
+{
+    public class EmployeeDatabaseSettings : IEmployeeDatabaseSettings
+    {
+        public string EmployeesCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IEmployeeDatabaseSettings
+    {
+         string EmployeesCollectionName { get; set; }
+         string ConnectionString { get; set; }
+         string DatabaseName { get; set; }
+    }
+}
